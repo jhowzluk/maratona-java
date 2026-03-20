dbfe50f612ed61f7cbeda6f68321b742d0f5f0ae
jhowzluk <jhonatanluksik@gmail.com>
2026-03-20 15:16:09 -0300

    feat: herança e atributo static/final

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("234234-232");
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Jhow");
+        pessoa.setCpf("1111111");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Lucas");
+        funcionario.setCpf("2222222");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000);
+        System.out.println("-------------");
+        funcionario.imprime();
+        funcionario.relatorioPagamento();
+
+    }
+}
